b21a03e2ac8544aeaba3c9f01a832ef6f638c8b4
Niklas Polke <niklas.polke@gmx.de>
2015-08-05 20:03:15 +0200

    Correct LoginFilter to not redirect css resource requests

--- src/main/java/de/training/filter/LoginFilter.java
+++ src/main/java/de/training/filter/LoginFilter.java
@@ -3,6 +3,7 @@ package de.training.filter;
 import java.io.IOException;
 import java.util.logging.Logger;
 
+import javax.faces.application.ResourceHandler;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -18,26 +19,25 @@ import de.training.beans.LoginBean;
 
 @WebFilter("/*")
 public class LoginFilter implements Filter {
-	
+
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
 			throws IOException, ServletException {
 		HttpServletRequest httpRequest = (HttpServletRequest) request;
 		Logger logger = Logger.getLogger(URILoggerFilter.class.getName());
-		
+
 		HttpSession session = httpRequest.getSession(false);
-		boolean loggedIn = false;
-		LoginBean login = null;
-		if (session != null) {
-			login = (LoginBean) session.getAttribute("loginBean");
-			if (login != null) {
-				loggedIn = login.isLoggedIn();
-			}
-		}
-		logger.info("logged in as " + (loggedIn ? login.getUser() : "---"));
-		if (loggedIn || httpRequest.getRequestURI().endsWith("login.xhtml")) {
+		LoginBean login = (session != null) ? (LoginBean) session.getAttribute("loginBean") : null;
+
+		boolean loggedIn = login != null && login.isLoggedIn();
+		boolean loginRequest = httpRequest.getRequestURI().equals(httpRequest.getContextPath() + "/login.xhtml");
+		boolean resourceRequest = httpRequest.getRequestURI()
+				.startsWith(httpRequest.getContextPath() + ResourceHandler.RESOURCE_IDENTIFIER);
+
+		if (loggedIn || loginRequest || resourceRequest) {
 			filterChain.doFilter(request, response);
 		} else {
+			logger.info("redirect to login page (original target: " + httpRequest.getRequestURI() + ")");
 			HttpServletResponse httpResponse = (HttpServletResponse) response;
 			httpResponse.sendRedirect(httpRequest.getContextPath() + "/login.xhtml");
 		}
@@ -46,7 +46,7 @@ public class LoginFilter implements Filter {
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
 	}
-	
+
 	@Override
 	public void destroy() {
 	}
