4dc3c3f6a22c7c1c2ca59f9efdfef8ba09281175
Niklas Polke <niklas.polke@gmx.de>
2015-08-09 13:43:33 +0200

    Set login to compare with database

--- src/main/java/de/training/beans/LoginBean.java
+++ src/main/java/de/training/beans/LoginBean.java
@@ -1,26 +1,33 @@
 package de.training.beans;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import de.training.entities.User;
 
 @ManagedBean
 @SessionScoped
 public class LoginBean {
-	
+
 	private String user;
 
     private String username;
-    
+
     private String password;
-    
+
     public String getUser() {
     	return user;
     }
-    
+
     public void setUser(String user) {
     	this.user = user;
     }
@@ -45,39 +52,48 @@ public class LoginBean {
     	Logger logger = Logger.getLogger(LoginBean.class.getName());
     	logger.info("trying to login as: " + username);
     	String redirect = null;
-    	
+
     	FacesContext fContext = FacesContext.getCurrentInstance();
 		if (username != null && username.length() > 0 && password != null && password.length() > 0) {
-    		//TODO:do the username/password check
-    		if (username.equals(password)) {
-    			// successfully logged in and redirect to start page
-    			setUser(username);
-    			logger.info("login succeeded");
-    			redirect = "index.xhtml?faces-redirect=true";
-    		} else {
-    			FacesMessage msgGlobal = new FacesMessage("Login failed");
-    			FacesMessage msgUsername = new FacesMessage("Login possibly unknown");
-    			FacesMessage msgPassword = new FacesMessage("Password possibly wrong");
-    			fContext.addMessage(null, msgGlobal);
-    			fContext.addMessage("username", msgUsername);
-    			fContext.addMessage("password", msgPassword);
-    		}
-    	} else {
-    		if (username == null || username.length() == 0) {
-    			FacesMessage msgUsername = new FacesMessage("Username empty");
-    			fContext.addMessage("username", msgUsername);
-    		}
-    		if (password == null || password.length() == 0) {
-    			FacesMessage msgPassword = new FacesMessage("Password empty");
-    			fContext.addMessage("password", msgPassword);
-    		}
-    	}
+			// Open a database connection:
+			EntityManagerFactory emf = Persistence.createEntityManagerFactory("training-jpa");
+			EntityManager em = emf.createEntityManager();
+
+			TypedQuery<User> loginValidationQuery = em.createNamedQuery("User.validateLogin", User.class);
+			loginValidationQuery.setParameter("login", username);
+			loginValidationQuery.setParameter("password", password);
+			List<User> loginResult = loginValidationQuery.getResultList();
+
+			/* TODO: should be size() == 1 but currently no guarantee of no duplicates */
+			if (loginResult != null && loginResult.size() > 0) {
+				// successfully logged in and redirect to start page
+				setUser(username);
+				logger.info("login succeeded");
+				redirect = "index.xhtml?faces-redirect=true";
+			} else {
+				FacesMessage msgGlobal = new FacesMessage("Login failed");
+				FacesMessage msgUsername = new FacesMessage("Login possibly unknown");
+				FacesMessage msgPassword = new FacesMessage("Password possibly wrong");
+				fContext.addMessage(null, msgGlobal);
+				fContext.addMessage("username", msgUsername);
+				fContext.addMessage("password", msgPassword);
+			}
+		} else {
+			if (username == null || username.length() == 0) {
+				FacesMessage msgUsername = new FacesMessage("Username empty");
+				fContext.addMessage("username", msgUsername);
+			}
+			if (password == null || password.length() == 0) {
+				FacesMessage msgPassword = new FacesMessage("Password empty");
+				fContext.addMessage("password", msgPassword);
+			}
+		}
 		if (redirect == null) {
 			logger.info("login failed");
 		}
-    	return redirect;
-    }
-	
+		return redirect;
+	}
+
 	public boolean isLoggedIn() {
 		return getUser() != null;
 	}
--- src/main/java/de/training/entities/User.java
+++ src/main/java/de/training/entities/User.java
@@ -6,11 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQuery;
 import javax.persistence.TableGenerator;
 
 @Entity
+@NamedQuery(name="User.validateLogin",
+	query="SELECT u FROM User u WHERE u.login = :login AND u.password = :password")
 public class User implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
 
 	@Id
@@ -22,31 +25,32 @@ public class User implements Serializable {
 		pkColumnValue="USER_ID", // Specify the primary key column value that would be considered as a primary key generator
 		allocationSize=1)
 	private long id;
-	
+
 	private String login;
-	
+
 	private String password;
-	
+
 	public User() {
 	}
-	
+
 	public User(final String login, final String password) {
 		this.login = login;
 		this.password = password;
 	}
-	
+
 	public long getId() {
 		return id;
 	}
-	
+
 	public String getLogin() {
 		return login;
 	}
-	
+
 	public String getPassword() {
 		return password;
 	}
-	
+
+	@Override
 	public String toString() {
 		return "User: (" + login + ", " + password + ")";
 	}
